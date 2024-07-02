0ed3c3725b9831c52a2bf9e59ddaf395ee6a9b38
Kentaro Barnes <130931962+KentoBaguetti@users.noreply.github.com>
2024-07-02 13:11:20 -0400

    Zed config

--- server/src/controllers/LLMController.ts
+++ server/src/controllers/LLMController.ts
@@ -5,70 +5,70 @@ import type { ChatCompletionMessageParam } from "openai/resources";
 // TODO: Change parameters for gpt to use more tokens and be more concise?
 
 class LLMController {
-	openai: OpenAI;
-	model: string;
-	OPENAI_API_KEY: string | undefined;
-	systemPrompt: ChatCompletionMessageParam;
-	messages: ChatCompletionMessageParam[];
+  openai: OpenAI;
+  model: string;
+  OPENAI_API_KEY: string | undefined;
+  systemPrompt: ChatCompletionMessageParam;
+  messages: ChatCompletionMessageParam[];
 
-	constructor(gptPrompt: string, model: string) {
-		this.OPENAI_API_KEY = process.env.OPENAI_KEY;
-		this.openai = new OpenAI({
-			apiKey: this.OPENAI_API_KEY,
-		});
-		this.model = model;
-		this.systemPrompt = {
-			role: "system",
-			content: gptPrompt,
-		};
-		this.messages = [];
-	}
+  constructor(gptPrompt: string, model: string) {
+    this.OPENAI_API_KEY = process.env.OPENAI_KEY;
+    this.openai = new OpenAI({
+      apiKey: this.OPENAI_API_KEY,
+    });
+    this.model = model;
+    this.systemPrompt = {
+      role: "system",
+      content: gptPrompt,
+    };
+    this.messages = [];
+  }
 
-	start(): void {
-		this.initiateArgument();
-	}
+  start(): void {
+    this.initiateArgument();
+  }
 
-	initiateArgument(): void {
-		this.messages.push(this.systemPrompt);
-	}
+  initiateArgument(): void {
+    this.messages.push(this.systemPrompt);
+  }
 
-	async addUserMessage(userInput: string): Promise<void> {
-		const inputData: ChatCompletionMessageParam = {
-			role: "user",
-			content: userInput,
-		};
-		this.messages.push(inputData);
-	}
+  async addUserMessage(userInput: string): Promise<void> {
+    const inputData: ChatCompletionMessageParam = {
+      role: "user",
+      content: userInput,
+    };
+    this.messages.push(inputData);
+  }
 
-	async gptResponse(): Promise<string | null | undefined> {
-		try {
-			const response = await this.openai.chat.completions.create({
-				model: this.model,
-				messages: this.messages,
-				max_tokens: 100,
-				temperature: 0.7,
-				frequency_penalty: 0.5,
-				presence_penalty: 0.0,
-			});
+  async gptResponse(): Promise<string | null | undefined> {
+    try {
+      const response = await this.openai.chat.completions.create({
+        model: this.model,
+        messages: this.messages,
+        max_tokens: 100,
+        temperature: 0.7,
+        frequency_penalty: 0.5,
+        presence_penalty: 0.0,
+      });
 
-			const assistantResponse: ChatCompletionMessageParam =
-				response.choices[0].message;
+      const assistantResponse: ChatCompletionMessageParam =
+        response.choices[0].message;
 
-			console.log(assistantResponse);
+      console.log(assistantResponse);
 
-			this.messages.push(assistantResponse);
+      this.messages.push(assistantResponse);
 
-			const stringResponse: string | null | undefined =
-				assistantResponse.content;
+      const stringResponse: string | null | undefined =
+        assistantResponse.content;
 
-			return stringResponse;
-		} catch (error: unknown) {
-			if (error instanceof Error) {
-				console.error(`Error recieving response from GPT: ${error.message}`);
-				return `Error recieving response from GPT: ${error.message}`;
-			}
-		}
-	}
+      return stringResponse;
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.error(`Error recieving response from GPT: ${error.message}`);
+        return `Error recieving response from GPT: ${error.message}`;
+      }
+    }
+  }
 }
 
 export default LLMController;
